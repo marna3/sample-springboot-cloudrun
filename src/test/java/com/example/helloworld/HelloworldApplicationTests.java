6f1ad98f273fa6805d089c6c6861f7716d9a5712
Takuma Ikeda <takuma.ikeda@hpe.com>
2024-07-01 14:19:22 +0900

    DELETE: 不要なパッケージをコメントアウト

--- src/test/java/com/example/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloworldApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.helloworld;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
