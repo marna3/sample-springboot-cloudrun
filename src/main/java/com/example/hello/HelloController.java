af6d08ceea4bed9e12665c5703e40840a493b97c
Takuma Ikeda <takuma.ikeda@hpe.com>
2025-02-05 17:23:42 +0900

    Update: DBからIDを検索して当てはまる情報を表示するアプリに変更

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +11,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @Autowired
+    private HelloService service;
+
+    @GetMapping("/")
     public String getHello() {
         // hello.htmlに画面遷移
         return "hello";
     }
-
-    @PostMapping("/hello")
-    public String postRequest(@RequestParam("text1") String str, Model model) {
-        
-        // 画面から受け取った文字列をModelに登録
-        model.addAttribute("sample", str);
-        
-        // response.htmlに画面遷移
-        return "hello/response";
-    }
     
+    @PostMapping("/hello/employee")
+    public String postDbRequest(@RequestParam("employee_id") String id, Model model) {
+
+        // 1件検索
+        Employee employee = service.getEmployee(id);
+
+        // 検索結果をModelに登録
+        model.addAttribute("employee", employee);
+
+        // employee.htmlに画面遷移
+        return "hello/employee";
+    }
 }
